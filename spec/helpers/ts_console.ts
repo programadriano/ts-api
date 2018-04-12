2d9c75a81338f8c8eaa849362d8ae4e169cc3fe5
tadriano <tadriano@tadriano.local>
2018-04-11 23:03:52 -0300

    add ts-console

---
+++ spec/helpers/ts_console.ts
@@ -0,0 +1,6 @@
+// tslint:disable-next-line:no-var-requires
+//http://bisaga.com/blog/programming/testing-typescript-node-app-with-jasmine/
+const TSConsoleReporter = require("jasmine-ts-console-reporter");
+
+jasmine.getEnv().clearReporters(); // Clear default console reporter
+jasmine.getEnv().addReporter(new TSConsoleReporter());
--- spec/test-spec.ts
+++ spec/test-spec.ts
@@ -1,24 +1,14 @@
 export class MyApi {
-    public getName(): string {
-        return "";
-    }
+  public getName(): string {
+    return "MyNam";
+  }
 }
 
-
 describe("MyApi getName function return value", () => {
-    it("Should be defined.", () => {
-        const myapi = new MyApi();
-        expect(myapi.getName()).toBeDefined("The function getName() should be defined.");
-    });
  
-    it("Should't return blank.", () => {
-        const myapi = new MyApi();
-        expect(myapi.getName()).not.toMatch("", "The function getName() should return the name.");
-    });
- 
-    it("Should return 'MyName'" , () => {
-        const myapi = new MyApi();
-        expect(myapi.getName()).toMatch("MyName");
-    });
- 
-} );
+
+   it("Should return 'MyName'", () => {
+    const myapi = new MyApi();
+    expect(myapi.getName()).toMatch("MyName");
+  });
+});
